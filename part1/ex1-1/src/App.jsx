cdcb6c437400edef53d1bba3c20774566f4ff027
Lautig <lautig45@gmail.com>
2025-08-13 23:02:41 -0300

    1.1

---
+++ part1/ex1-1/src/App.jsx
@@ -0,0 +1,53 @@
+const Header = (course) => {
+  console.log(course)
+return (  
+  <h1>
+    {course.course}
+  </h1>
+  )
+}
+const Content = (parts) => {
+  console.log(parts)
+  return (
+    <div>
+      <p>
+        {parts.name} {parts.exercises}
+      </p>
+    </div>
+  )
+}
+const Total = (exercises) => {  
+  console.log(exercises)
+  return (
+    <div>
+      <p>
+        Number of exercises: {exercises.exercises1 + exercises.exercises2 + exercises.exercises3}
+      </p>
+    </div>
+  )
+
+}
+
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content name={part1} exercises={exercises1} />
+      <Content name={part2} exercises={exercises2} />
+      <Content name={part3} exercises={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+    </div>
+  )
+}
+
+export default App
