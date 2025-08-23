47587bdd1c0cf020803659032fca77b4204016ff
Lautig <lautig45@gmail.com>
2025-08-22 23:37:18 -0300

    1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -46,6 +46,9 @@ const App = () => {
       <Display text="good " value={good} />
       <Display text="neutral " value={neutral} />
       <Display text="bad " value={bad} />
+      <Display text="all " value={good + neutral + bad} /> 
+      <Display text="average " value={(good - bad) / (good + neutral + bad)} />
+      <Display text="positive " value={ (good / (good + neutral + bad)) * 100 + ' %'} />
     </div>
   )
 }
